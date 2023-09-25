e0eb5a70a2b8a45fa708a118d22fa5f2a23205f9
Klau MT <klau.mt@gmail.com>
2023-09-24 22:06:18 -0600

    Improve animations

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { useRive, useStateMachineInput} from '@rive-app/react-canvas';
+import { useRive, useStateMachineInput } from '@rive-app/react-canvas';
 
 const A1 = () => {
     const { rive, RiveComponent } = useRive({
@@ -8,7 +8,7 @@ const A1 = () => {
         artboard: 'animacion-home',
         stateMachines: 'animaciones',
     });
-    
+
     return (
         <RiveComponent
             className="canvas-size"
@@ -28,8 +28,6 @@ const A2 = () => {
     return (
         <RiveComponent
             className="canvas-size"
-            onMouseEnter={() => rive && rive.play()}
-            onMouseLeave={() => rive && rive.pause()}
         />
     );
 }
@@ -45,26 +43,32 @@ const A3 = () => {
     return (
         <RiveComponent
             className="canvas-size"
-            onMouseEnter={() => rive && rive.play()}
-            onMouseLeave={() => rive && rive.pause()}
         />
     );
 }
 
 
 const A4 = () => {
+    const STATE_MACHINE_NAME = 'animaciones'
     const { rive, RiveComponent } = useRive({
         src: 'roble.riv',
         autoplay: true,
         artboard: 'check',
-        
+        stateMachines: STATE_MACHINE_NAME,
+
     });
+    const check = useStateMachineInput(
+        rive,
+        STATE_MACHINE_NAME,
+        'check'
+    );
     return (
-        <RiveComponent
-            className="canvas-size"
-            onMouseEnter={() => rive && rive.play()}
-            onMouseLeave={() => rive && rive.pause()}
-        />
+        <div onClick={() => rive && check.fire()}>
+            <RiveComponent
+                className="canvas-size-check"
+            />
+            <button type="button" onClick={() => check.fire()}>Check</button>
+        </div>
     );
 }
 
@@ -72,11 +76,12 @@ const A4 = () => {
 const A5 = () => {
     const STATE_MACHINE_NAME = 'animaciones'
     const WHITE = 'white'
-    const GRAY = 'grey'
+    const GRAY = 'gray'
     const RED = 'red_gray'
     const YELLOW = 'yellow_gray'
     const WG = 'white_gray'
-    const { rive, RiveComponent } = useRive({
+    const canvas_style = "background-color: #c9c9c9c9"
+    const { rive, RiveComponent: RiveComponentTouch } = useRive({
         src: 'roble.riv',
         autoplay: true,
         artboard: 'loader',
@@ -101,44 +106,73 @@ const A5 = () => {
         rive,
         STATE_MACHINE_NAME,
         RED,
-        true
     );
     const wg = useStateMachineInput(
         rive,
         STATE_MACHINE_NAME,
         WG
     );
+    const changeColor = (color) => {
+        if (color === WHITE) {
+            white.fire();
+        } else if (color === GRAY) {
+            gray.fire();
+        } else if (color === RED) {
+            red.fire();
+        } else if (color === YELLOW) {
+            yellow.fire();
+        } else if (color === WG) {
+            wg.fire();
+        }
+    };
+
     return (
-        <RiveComponent
-            className="canvas-size"
-            onMouseEnter={() => rive && rive.play()}
-            onMouseLeave={() => rive && rive.pause()}
-        />
+        <div >
+            <RiveComponentTouch
+                className="canvas-size gray-background"
+            />
+            <select onChange={(event) => { changeColor(event.target.value) }}>
+                <option>{WHITE}</option>
+                <option>{GRAY}</option>
+                <option selected>{RED}</option>
+                <option>{YELLOW}</option>
+                <option>{WG}</option>
+            </select>
+        </div>
     );
 }
 const A6 = () => {
+    const STATE_MACHINE_NAME = 'animaciones';
+    const BACK = 'back';
+    const FORWARD = 'forward'
     const { rive, RiveComponent } = useRive({
         src: 'roble.riv',
         autoplay: true,
         artboard: 'timeline',
-        stateMachines: 'animaciones',
+        stateMachines: STATE_MACHINE_NAME,
     });
+    const backInput = useStateMachineInput(rive, STATE_MACHINE_NAME, BACK);
+    const forwardInput = useStateMachineInput(rive, STATE_MACHINE_NAME, FORWARD);
     return (
-        <RiveComponent
-            className="canvas-size"
-            onMouseEnter={() => rive && rive.play()}
-            onMouseLeave={() => rive && rive.pause()}
-        />
+        <div>
+            <RiveComponent
+                className="timeline-canvas-size"
+            />
+            <button type="button" onClick={() => backInput.fire()}>Back</button>
+            <button type="button" onClick={() => forwardInput.fire()}>Forward</button>
+        </div>
     );
 }
 
 const container = document.getElementById("app");
 const root = createRoot(container)
-root.render(<div>
-    <A1 />
-    <A2 />
-    <A3 />
-    <A4 />
-    <A5 />
-    <A6 />
-</div>);
+root.render(
+    <div>
+        <A1 />
+        <A2 />
+        <A3 />
+        <A4 />
+        <A5 />
+        <A6 />
+    </div>
+);
